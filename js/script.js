429c8ed036bd3888825ab1f532ec7da858273bee
dusiko <zheka_yura@ukr.net>
2022-03-21 12:00:26 +0200

    little fix

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 function copyCardNum(event) {
 	if (event.target.classList.contains('card__num')) {
-		const test = event.target.nextElementSibling
-		test.select()
-		test.blur()
-		document.execCommand("copy")
-		const target = event.target
-		const text = target.innerText
-		target.innerText = 'номер скопірований!'
-		target.style.color = 'green'
-		setTimeout(() => {
-			target.style.color = '#212121'
-			target.innerText = text
-		}, 1000)
+		if (event.target.innerText !== 'номер скопірований!') {
+			const test = event.target.nextElementSibling
+			test.select()
+			test.blur()
+			document.execCommand("copy")
+			const target = event.target
+			const text = target.innerText
+			target.innerText = 'номер скопірований!'
+			target.style.color = 'green'
+			setTimeout(() => {
+				target.style.color = '#212121'
+				target.innerText = text
+			}, 1000)
+		}
 	}
 }
 
