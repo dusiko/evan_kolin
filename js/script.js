08b4de6dda4568fa33a8013bd6f96e6cda3ff887
dusiko <zheka_yura@ukr.net>
2022-04-01 10:01:14 +0300

    video_block scripts

--- js/script.js
+++ js/script.js
@@ -44,3 +44,58 @@ function showNextVideoByScrollingPage() {
 		window.removeEventListener('scroll', getPix)
 	}
 }
+
+/* iframe video by click
+========================== */
+
+function findVideos() {
+	let videos = document.querySelectorAll('.video-block__item');
+
+	for (let i = 0; i < videos.length; i++) {
+		setupVideo(videos[i]);
+	}
+}
+
+function setupVideo(video) {
+	let link = video.querySelector('.video-block__item-link')
+	let media = video.querySelector('.video-block__item-media')
+	let button = video.querySelector('.video-block__item-button')
+	let id = parseMediaURL(media)
+
+	video.addEventListener('click', () => {
+		let iframe = createIframe(id);
+
+		link.remove();
+		button.remove();
+		video.appendChild(iframe);
+	});
+
+	link.removeAttribute('href');
+	video.classList.add('video-block__item--enabled');
+}
+
+function parseMediaURL(media) {
+	let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)/i;
+	let url = media.src;
+	let match = url.match(regexp);
+
+	return match[1]
+}
+
+function createIframe(id) {
+	let iframe = document.createElement('iframe')
+
+	iframe.setAttribute('allowfullscreen', '');
+	iframe.setAttribute('src', generateURL(id));
+	iframe.classList.add('video__media');
+
+	return iframe
+}
+
+function generateURL(id) {
+	let query = '?rel=0&showinfo=0&autoplay=1';
+
+	return 'https://www.youtube.com/embed/' + id + query;
+}
+
+findVideos();
