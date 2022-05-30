24009ae4ea790fe0de257efb21cfe8c7504c9579
Zheka Yurii <zheka_yura@ukr.net>
2022-05-30 09:18:49 +0300

    default v 3.0

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function copyCardNum(event) {
 			range.selectNode(target);
 			window.getSelection().removeAllRanges(); // clear current selection
 			window.getSelection().addRange(range); // to select text
-			document.execCommand("copy");
+			window.navigator.clipboard.writeText(text);
 			window.getSelection().removeAllRanges();// to deselect
 			target.innerText = 'номер скопірований!'
 			target.style.color = 'green'
@@ -19,24 +19,4 @@ function copyCardNum(event) {
 			}, 1000)
 		}
 	}
-}
-
-function showAllVideo() {
-	const srcArr = ['https://www.youtube.com/embed/0mAsaUGLbbk', 'https://www.youtube.com/embed/d4O2AQ0wrXY', 'https://www.youtube.com/embed/8mSHN9654KE', 'https://www.youtube.com/embed/wziya1qpTtg']
-	const $Parent = document.querySelector('.video-block__inner')
-	$Parent.removeChild(this.event.target)
-	for (let i = 0; i < srcArr.length; i++) {
-		const $div = document.createElement('DIV')
-		$div.classList.add('video-block__item')
-		const $iframe = document.createElement('iframe')
-		$iframe.classList.add('video-block__item-video')
-		$iframe.setAttribute('src', srcArr[i])
-		$iframe.setAttribute('title', 'YouTube video player')
-		$iframe.setAttribute('frameborder', '0')
-		$iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
-		$iframe.setAttribute('allowfullscreen', 'allowfullscreen')
-		$div.appendChild($iframe)
-		$Parent.appendChild($div)
-	}
-
 }
